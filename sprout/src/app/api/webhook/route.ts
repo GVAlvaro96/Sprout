db2f59d48bd7d9403a8f3ce10d613363ea316d58
GVAlvaro96 <alvaro.garciavelasco96@gmail.com>
2026-05-02 18:08:34 +0200

    fix: update Stripe API version in webhook route
    - Changed Stripe API version from '2024-04-10' to '2026-04-22.dahlia' to ensure compatibility with the latest features and improvements.

--- sprout/src/app/api/webhook/route.ts
+++ sprout/src/app/api/webhook/route.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe'
 import { createClient } from '@supabase/supabase-js'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10',
+    apiVersion: '2026-04-22.dahlia' as any,
 })
 
 // Cliente de Supabase con privilegios de ADMIN (Service Role Key)
